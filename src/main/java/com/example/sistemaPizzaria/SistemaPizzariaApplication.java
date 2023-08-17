8fb5d633116dec78ef7790f4dc263441b4d2b141
guleao <guleaosoares@gmail.com>
2023-08-17 12:56:35 -0300

    Primeiro commit - sistemaPizzaria

---
+++ src/main/java/com/example/sistemaPizzaria/SistemaPizzariaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemaPizzaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPizzariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPizzariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sistemaPizzaria/SistemaPizzariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemaPizzaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPizzariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
